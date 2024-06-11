9f71c251652efdb24004328e29b95674f7cc3f1c
DjonniStorm <i.pazushkin@yandex.ru>
2024-06-11 22:49:09 +0400

    3.3

--- var3.cs
+++ var3.cs
@@ -153,3 +153,160 @@ class Task1
         return $"{counter}";
     }
 }
+
+//№3
+private void button1_Click(object sender, EventArgs e)
+{
+    string userText = textBoxMethod1.Text;
+    //просто чтобы видеть значение которое ввёл пользователь
+    listBoxUserData.Items.Insert(listBoxUserData.Items.Count, userText);
+    
+    var a = new Task3List(Convert.ToInt32(userText));
+    
+    //вставляем результат от метода1
+    listBoxResult.Items.Insert(listBoxResult.Items.Count, a.method());
+    
+    //посмотреть что там за массив то был
+    listBoxRandomArray.Items.Insert(listBoxRandomArray.Items.Count, a.Text);
+
+    //второй метод и результат от него
+    listBoxResult.Items.Insert(listBoxResult.Items.Count, a.method(Convert.ToInt32(textBoxMethod2.Text)));
+
+}
+class Task1 : ITask3
+{
+    Random random = new();
+    private int[] array;
+    private string text;
+    // свойсто чтобы получить какой массив там был
+    public string Text
+    { 
+        get => text;
+    }
+    public Task1(int n)
+    {
+        n = 
+            n > 10 ? 
+            n 
+            : 
+            random.Next(10, 20);
+        array = new int[n];
+        text = "";
+        //заполняем рандомом
+        for (int i = 0; i < n; i++) array[i] = random.Next(-10, 10);
+        //просто чтобы понять, что за массив получился
+        foreach (int i in array) text += $"{i}  ";
+    }
+    /// <summary>
+    /// сумма положительных элементов
+    /// </summary>
+    /// <returns></returns>
+    public string method()
+    {
+        int sum = 0;
+        foreach (int i in array)
+        {
+            if (i > 0)
+            {
+                sum += i;
+            }
+        }
+        return $"{sum}";
+    }
+    /// <summary>
+    /// Количество нечётных элементов, меньше пользовательского числа
+    /// </summary>
+    /// <param name="userValue">пользовательское число</param>
+    /// <returns></returns>
+    public string method(int userValue)
+    {
+        //задание
+        int counter = 0;
+        foreach (int i in array)
+        {
+            if (i < userValue && i % 2 != 0) counter++;
+        }
+        return $"{counter}";
+    }
+    public string this[int index]
+    {
+        get
+        {
+            if (index > 0 && index < array.Length)
+            {
+                return array[index].ToString();
+            }
+            return "";
+        }
+    }
+}
+interface ITask3
+{
+    string method();
+    string method(int userValue);
+    string this[int index]
+    {
+        get;
+    }        
+}
+class Task3List : ITask3
+{
+    Random random = new();
+    List<int> list;
+    private string text;
+    // свойсто чтобы получить какой массив там был
+    public string Text
+    {
+        get => text;
+    }
+    public Task3List(int n)
+    {
+        n =
+            n > 10 ?
+            n
+            :
+            random.Next(10, 20);
+        list = new(n);
+        //заполняем рандомом
+        for (int i = 0; i < n; i++) list.Add(random.Next(-10, 10));
+        //просто чтобы понять, что за массив получился
+        foreach (int i in list) text += $"{i}  ";
+    }
+    public string this[int index]
+    {
+        get
+        {
+            if (index > 0 && index < list.Count)
+            {
+                return list[index].ToString();
+            }
+            return "";
+        }
+    }
+
+    public string method()
+    {
+        int sum = 0;
+        foreach (int i in list)
+        {
+            if (i > 0)
+            {
+                sum += i;
+            }
+        }
+        return $"{sum}";
+    }
+
+    public string method(int userValue)
+    {
+        //задание
+        int counter = 0;
+        foreach (int i in list)
+        {
+            if (i < userValue && i % 2 != 0) counter++;
+        }
+        return $"{counter}";
+    }
+}
+
+//
